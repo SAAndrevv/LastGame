900e29e6d8287e7f62a529bee3dfe3974ed5d1fb
Andrey Sudomoykin <SAAndrew52@yandex.ru>
2021-05-21 13:13:51 +0300

    commit

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public GameObject GO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal  = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other) {
+        if  (other.gameObject.CompareTag("Pick Up")){
+            Destroy(other.gameObject);
+
+            Instantiate(GO);
+            GO.transform.position = new Vector3(Random.Range(-14f, 14f), 0.5f, Random.Range(-14f, 14f));
+            
+        }
+        
+    }
+}
